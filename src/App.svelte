b451c3bb63021ed5c0b6bf3c4772fdb4a04dcfdd
JhoanaVicente <126072279+JhoanaVicente@users.noreply.github.com>
2024-04-12 11:04:57 +0200

    agregando componente grafico

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,16 @@
 <script>
     import Tabla from './lib/tabla.svelte';
-  
+    import Grafico from './lib/grafico.svelte';
+    
 </script>
 
 <main>
   <Tabla />
+
+  <Grafico />
+  
 </main>
+
 
 <style>
   /* Estilos opcionales para el contenedor principal */
